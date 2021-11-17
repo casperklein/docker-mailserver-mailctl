0753894df831071734b56cffd3372f2e9c4256d8
casperklein <casperklein@users.noreply.github.com>
2021-11-17 15:31:57 +0100

    'config' option added

--- mailctl-completion.bash
+++ mailctl-completion.bash
@@ -2,7 +2,7 @@ _mailctl() {
 	local cur prev options
 	cur=${COMP_WORDS[COMP_CWORD]}
 	prev=${COMP_WORDS[COMP_CWORD-1]}
-	options="status start stop restart setup queue flush view delete fail2ban ports postconf logs login supervisor update-check update-packages versions"
+	options="config delete fail2ban flush login logs ports postconf queue restart setup start status stop supervisor update-check update-packages versions view"
 	if [ $COMP_CWORD -eq 1 ]; then
 		COMPREPLY=( $(compgen -W "${options[@]}" -- $cur) )
 	elif [ $COMP_CWORD -eq 2 ]; then
