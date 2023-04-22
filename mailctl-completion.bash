ba4533d2efc37c3620b0b2dc4de9c97b8c4cadd4
casperklein <casperklein@users.noreply.github.com>
2023-04-22 23:57:20 +0200

    add new 'onhold' command

--- mailctl-completion.bash
+++ mailctl-completion.bash
@@ -2,16 +2,22 @@ _mailctl() {
 	local cur prev options
 	cur=${COMP_WORDS[COMP_CWORD]}
 	prev=${COMP_WORDS[COMP_CWORD-1]}
-	options="config delete fail2ban flush login logs ports postconf queue restart setup start status stop supervisor update-check update-packages versions view"
+	options="config delete fail2ban flush login logs ports postconf queue restart setup start status stop supervisor unhold update-check update-packages versions view"
 	if [ $COMP_CWORD -eq 1 ]; then
 		COMPREPLY=( $(compgen -W "${options[@]}" -- $cur) )
 	elif [ $COMP_CWORD -eq 2 ]; then
 		case "$prev" in
+			delete)
+				COMPREPLY=( $(compgen -W "ALL" -- $cur) )
+				;;
 			fail2ban)
 				COMPREPLY=( $(compgen -W "ban unban" -- $cur) )
 				;;
 			logs)
 				COMPREPLY=( $(compgen -W "-f" -- $cur) )
+				;;
+			unhold)
+				COMPREPLY=( $(compgen -W "ALL" -- $cur) )
 				;;
 		esac
 	fi
