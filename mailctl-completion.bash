b65d822ccaaf5bf4e9f19ae1634cf9df64c34805
casperklein <casperklein@users.noreply.github.com>
2025-02-18 00:22:50 +0100

    update bash-completion

--- mailctl-completion.bash
+++ mailctl-completion.bash
@@ -1,23 +1,26 @@
 _mailctl() {
-	local cur prev options
+	local cur prev args
+
 	cur=${COMP_WORDS[COMP_CWORD]}
 	prev=${COMP_WORDS[COMP_CWORD-1]}
-	options="config delete fail2ban flush login logs ports postconf queue restart setup start status stop supervisor unhold update-check update-packages versions view"
-	if [ $COMP_CWORD -eq 1 ]; then
-		COMPREPLY=( $(compgen -W "${options[@]}" -- $cur) )
-	elif [ $COMP_CWORD -eq 2 ]; then
+
+	if (( COMP_CWORD == 1 )); then
+		args="config delete fail2ban flush login logs ports postconf processes queue restart setup start status stop supervisor unhold update-check update-packages versions view"
+		COMPREPLY=( $(compgen -W "$args" -- $cur) )
+
+	elif (( COMP_CWORD == 2 )); then
 		case "$prev" in
 			delete)
-				COMPREPLY=( $(compgen -W "ALL" -- $cur) )
+				COMPREPLY=( $(compgen -W "ALL" -- "$cur") )
 				;;
 			fail2ban)
-				COMPREPLY=( $(compgen -W "ban unban log" -- $cur) )
+				COMPREPLY=( $(compgen -W "ban unban log" -- "$cur") )
 				;;
 			logs)
-				COMPREPLY=( $(compgen -W "-f" -- $cur) )
+				COMPREPLY=( $(compgen -W "-f" -- "$cur") )
 				;;
 			unhold)
-				COMPREPLY=( $(compgen -W "ALL" -- $cur) )
+				COMPREPLY=( $(compgen -W "ALL" -- "$cur") )
 				;;
 		esac
 	fi
